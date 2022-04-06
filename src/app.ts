028b9b3ea2922598031cf4a660203602b80fb312
Akseli Kolari <akzuu@pm.me>
2022-04-06 15:30:00 +0300

    Creating some logic for the telegram bot

--- src/app.ts
+++ src/app.ts
@@ -1,16 +1,23 @@
+import "dotenv/config";
 import initServer from "./components/api/server";
 import { log } from "./lib/log";
-import 'dotenv/config';
-
+import { bot, messageHandler } from "./components/bot/bot";
 
 module.exports = (async () => {
   try {
     const server = await initServer();
     await server.start();
   } catch (error) {
-    log.error('Error starting server!', error);
+    log.error("Error starting server!", error);
     process.exit(1);
   }
 
-  log.info('Lakka started successfully!');
+  try {
+    bot.onText(/\/\/*./, messageHandler);
+  } catch (error) {
+    log.error("Error starting the command bot!", error);
+    process.exit(1);
+  }
+
+  log.info("Lakka started successfully!");
 })();
--- src/components/bot/bot.ts
+++ src/components/bot/bot.ts
@@ -1,5 +1,45 @@
-import TelegramBot from 'node-telegram-bot-api';
+// https://github.com/yagop/node-telegram-bot-api/issues/319
+process.env.NTBA_FIX_319 = "1";
+
+import TelegramBot from "node-telegram-bot-api";
 
 const TG_TOKEN = process.env.TG_TOKEN;
+if (!TG_TOKEN) throw new Error("Missing env variable TG_TOKEN!");
+
+export const AVAILABLE_COMMANDS = [
+  ["help", "Show this message"],
+  ["createStream", "Starts the stream creation process"],
+];
 
 export const bot = new TelegramBot(TG_TOKEN, { polling: true });
+
+export const messageHandler = (
+  msg: TelegramBot.Message,
+  match: RegExpExecArray | null
+) => {
+  const command = msg.text?.slice(1);
+  const chatId = msg.chat.id;
+  switch (command) {
+    default:
+      sendMessage(
+        chatId,
+        `Unknown command: ${command}.\nUse /help to get a list of available commands`
+      );
+      break;
+    case "help":
+      sendMessage(
+        chatId,
+        `Available commands:\n${AVAILABLE_COMMANDS.map(
+          (e) => `/${e[0]} ${e[1]}`
+        ).join("\n")}`
+      );
+      break;
+    case "createStream":
+      sendMessage(chatId, "Not available, yet");
+      break;
+  }
+};
+
+export const sendMessage = (chatId: TelegramBot.ChatId, msg: string) => {
+  bot.sendMessage(chatId, msg);
+};
