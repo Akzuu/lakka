349ebeb43f5b85aebc8a27e0de4b5b689125ec7d
Akseli Kolari <akzuu@pm.me>
2022-04-14 13:57:48 +0300

    Setting up docker compose for database

--- environment.d.ts
+++ environment.d.ts
@@ -2,7 +2,11 @@ declare global {
   namespace NodeJS {
     interface ProcessEnv {
       TG_TOKEN: string;
-      NODE_ENV: 'development' | 'production';
+      NODE_ENV: "development" | "production";
+      MYSQL_DATABASE: string;
+      MYSQL_USER: string;
+      MYSQL_PASSWORD: string;
+      MYSQL_ROOT_PASSWORD: string;
       PORT?: string;
     }
   }
